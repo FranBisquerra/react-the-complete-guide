44f4e323f926ad19268e1f5d4ae2ce17c88605b6
fbisquerra <fbisquerra@travelcompositor.com>
2020-03-16 22:00:29 +0100

    Add first component Person

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Person from './Person/Person';
 
-function App() {
-  return (
-    <div>
-      <h1>Hi, I'm a react app</h1>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi!, Iḿ a react App</h1>
+        <Person/>
+      </div>
+    )
+  }
 }
 
 export default App;
