6720d657e144fd193f4c9ffc06b509c24609b6cf
fbisquerra <fbisquerra@travelcompositor.com>
2020-03-17 12:43:51 +0100

    Add state and make data variable

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import React, { Component } from 'react';
 import Person from './Person/Person';
 
 class App extends Component {
+
+  state = {
+    persons: [
+      {name: 'Max', age: 30},
+      {name: 'Fran', age: 29},
+      {name: 'Stephanie', age: 26},
+    ]
+  } 
+
   render() {
     return (
       <div className="App">
         <h1>Hi!, I'm a react App</h1>
-        <Person name="Max" age="30"/>
-        <Person name="Fran" age="29">My hobbies: Racing</Person>
-        <Person name="Stephanie" age="26"/>
+        <button>Switch name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     )
   }
