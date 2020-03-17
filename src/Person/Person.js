e252f29af1b1dfac4f6a1232d13ab36d24df117d
fbisquerra <fbisquerra@travelcompositor.com>
2020-03-17 12:24:31 +0100

    Output dinamic content

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const person = () => <p>I'm a person</p>;
+const person = () => <p>I'm a person and I'm {Math.floor(Math.random() * 30)} years old</p>;
 
 export default person;
