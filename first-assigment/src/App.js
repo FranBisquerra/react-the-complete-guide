90c308222ff5047e827ed768fc2bb6c22b594172
fbisquerra <fbisquerra@travelcompositor.com>
2020-03-17 18:50:07 +0100

    Add change event handler when typing

--- first-assigment/src/App.js
+++ first-assigment/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import UserOutput from './UserOutput/UserOutput'
+import UserInput from './UserInput/UserInput';
 import './App.css';
 
 class App extends Component {
@@ -8,9 +9,20 @@ class App extends Component {
     username: 'Pepe'
   }
 
+  changedUsernameHandler = (event) => {
+    this.setState(
+      {
+        username: event.target.value
+      }
+    )
+  }
+
   render() {
     return(
       <div className="App">
+      <UserInput changed={this.changedUsernameHandler}></UserInput>
+      <br/>
+      <br/>
       <UserOutput username={this.state.username}></UserOutput>
       <UserOutput username={this.state.username}></UserOutput>
     </div>
