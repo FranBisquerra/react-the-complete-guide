bf5cf19bddac0b3e2c6d3326d415101a53dc23b0
fbisquerra <fbisquerra@travelcompositor.com>
2020-03-17 18:29:48 +0100

    Add UserOutput component

---
+++ first-assigment/src/UserOutput/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserOutput = () => {
+  return (
+    <div>
+      <p>Hey I'm paragraph 1</p>
+      <p>And down here it's me paragraph 2</p>
+    </div>
+  );
+}
+
+export default UserOutput;
